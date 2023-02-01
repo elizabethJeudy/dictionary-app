f3d64175e560b15c9e1514da99f4aa0fbb717a19
Elizabeth <tjeudy.e@gmail.com>
2023-02-01 11:32:44 -0500

    added and styled footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+export default function Footer() {
+	return (
+		<footer>
+			<p>
+				Open Sourced by:{" "}
+				<a
+					href="https://github.com/elizabethJeudy/dictionary-app"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Elizabeth Jeudy
+				</a>
+			</p>
+		</footer>
+	);
+}
