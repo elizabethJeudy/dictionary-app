1e3acb7910cbbf33dadf284d9721efad0655f04d
Elizabeth <tjeudy.e@gmail.com>
2023-02-02 22:41:02 -0500

    created search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
 
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+
 export default function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Search />
+			<Footer />
+		</div>
+	);
 }
---
+++ src/components/Search.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Search.css";
+import { SlMagnifier } from "react-icons/sl";
+
+export default function Search() {
+	// displays when searching for keyword
+	let [keyword, setKeyword] = useState("");
+	function search(event) {
+		event.preventDefault();
+		alert(`Searching for ${keyword} definition`);
+	}
+
+	// pulls the keyword typed in search
+	function handleKeywordChange(event) {
+		setKeyword(event.target.value);
+	}
+
+	return (
+		<div className="container searchContainer">
+			<h1>Dictionary</h1>
+
+			<form onSubmit={search}>
+				<input
+					type="search"
+					//
+					onChange={handleKeywordChange}
+					placeholder="What word would you like to search?"
+					className="search-input"
+				/>
+			</form>
+		</div>
+	);
+}
