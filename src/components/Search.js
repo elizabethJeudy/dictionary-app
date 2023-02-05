5eaf85ef14bb30f268a7eeef9d4023ccf747b48e
Elizabeth <tjeudy.e@gmail.com>
2023-02-04 23:07:34 -0500

    created search results component
    renders word user searches and return meaning component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
-import SearchResults from "./components/SearchResults";
+import SearchResults from "./SearchResults";
 
 // api documentation link: https://dictionaryapi.dev/
 
 export default function Search() {
+	// this state retrieves the word user searched
 	let [keyword, setKeyword] = useState("");
+
+	// this state updates whenever user searches
+	let [results, setResults] = useState(null);
 
 	// grabs the searched word from api
 	function handleResponse(response) {
 		console.log(response.data[0]);
 		console.log(response.data[0].meanings);
+		setResults(response.data[0]);
 	}
 
 	// displays when searching for keyword
@@ -27,20 +32,20 @@ export default function Search() {
 		setKeyword(event.target.value);
 	}
 
+	// renders whenever user searches for a word
+	// {results} receives results from API
 	return (
 		<div className="container searchContainer">
 			<h1>Dictionary</h1>
-
 			<form onSubmit={search}>
 				<input
 					type="search"
-					//
 					onChange={handleKeywordChange}
 					placeholder="What word would you like to search?"
 					className="search-input"
 				/>
 			</form>
-			<SearchResults />
+			<SearchResults results={results} />
 		</div>
 	);
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,22 @@
 import React from "react";
+import Meaning from "./Meaning";
+import "./SearchResults.css";
+import { RxSpeakerLoud } from "react-icons/rx";
 
-export default function SearchResults() {
-	return <div className=""></div>;
+export default function SearchResults(props) {
+	if (props.results) {
+		return (
+			<div className="SearchResults">
+				<RxSpeakerLoud />
+				<h2> {props.results.word.toUpperCase()} </h2>
+				{props.results.meanings.map(function (meaning, index) {
+					return (
+						<div key={index}>
+							<Meaning meaning={meaning} />
+						</div>
+					);
+				})}
+			</div>
+		);
+	} else return null;
 }
