27eea97afe257980badefab552d20840bd5e75e9
Elizabeth <tjeudy.e@gmail.com>
2023-02-08 13:43:16 -0500

    added phonetics component
    currently only renders text

---
+++ src/components/Phonetics.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Phonetics(props) {
+	return <div className="Phonetics">{props.phonetics.text}</div>;
+}
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 import "./SearchResults.css";
 import { RxSpeakerLoud } from "react-icons/rx";
 
@@ -7,8 +8,19 @@ export default function SearchResults(props) {
 	if (props.results) {
 		return (
 			<div className="SearchResults">
-				<RxSpeakerLoud />
+				<button>
+					<RxSpeakerLoud />
+				</button>
+
 				<h2> {props.results.word.toUpperCase()} </h2>
+				{props.results.phonetics.map(function (phonetics, index) {
+					return (
+						<div key={index}>
+							<Phonetics phonetics={phonetics} />
+						</div>
+					);
+				})}
+
 				{props.results.meanings.map(function (meaning, index) {
 					return (
 						<div key={index}>
