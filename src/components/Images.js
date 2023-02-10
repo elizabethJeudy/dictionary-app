b98df5cee7f9699186a84bf27900c6d29d7e78be
Elizabeth <tjeudy.e@gmail.com>
2023-02-09 21:37:45 -0500

    created the images component
    renders 3 images

---
+++ src/components/Images.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Images.css";
+
+export default function Images(props) {
+	if (props.images) {
+		console.log(props.images);
+		return (
+			<div className="Images">
+				<div className="row">
+					{props.images.map(function (photo, index) {
+						return (
+							<div className="col-3" key={index}>
+								<a href="{photo.src.original}" target="_blank">
+									<img
+										src={photo.src.medium}
+										alt="searched word"
+										className="img-fluid"
+									/>
+								</a>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
+}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 import axios from "axios";
 import SearchResults from "./SearchResults";
-
+import Images from "./Images";
 // api documentation link: https://dictionaryapi.dev/
 
 export default function Search(props) {
-	// this state retrieves the word user searched
+	// this state retrieves the word user searched and displays default word
 	let [keyword, setKeyword] = useState(props.defaultKeyword);
 	// this state updates whenever user searches
 	let [results, setResults] = useState(null);
 	// checks if user has searched a word
 	let [loaded, setLoaded] = useState(false);
+	// keeps track of photos
+	let [images, setImages] = useState(null);
+
 	// grabs the searched word from api
-	function handleResponse(response) {
+	function handleDictionaryResponse(response) {
 		setResults(response.data[0]);
+	}
+
+	function handlePexelsResponse(response) {
+		console.log(response.data);
+		setImages(response.data.photos);
 	}
 
 	// displays when searching for keyword
 	function search() {
-		// creates API key when user searches for a word
+		// creates API key when user searches for a word (dictionary api)
 		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-		axios.get(apiUrl).then(handleResponse);
+		axios.get(apiUrl).then(handleDictionaryResponse);
+		// images api
+		let pexelsApiKey =
+			"Qxfq5tSwsRmhhqGVN5pDBVG9CtF4B9Glf2CiW9eMlCHVZpT5VqHNQ9uI";
+		let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=4`;
+		// authorization for apikey, solution documentation here
+		let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+		axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
 	}
 
 	// calls API url to retrieve results
@@ -56,6 +71,7 @@ export default function Search(props) {
 				<div className="hint">
 					Suggested words: books, wine, yoga, sunshine....
 				</div>
+				<Images images={images} />
 				<SearchResults results={results} />
 			</div>
 		);
