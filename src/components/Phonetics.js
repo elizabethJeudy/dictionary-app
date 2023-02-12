56a9d327720989b603cb6a104edbf4b659f3125d
Elizabeth <tjeudy.e@gmail.com>
2023-02-12 11:30:49 -0500

    added audio to searched word

--- src/components/Phonetics.js
+++ src/components/Phonetics.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { RxSpeakerLoud } from "react-icons/rx";
 
 export default function Phonetics(props) {
-	return <div className="Phonetics">{props.phonetics.text}</div>;
+	return (
+		<div className="Phonetics">
+			{props.phonetics.text}
+			<a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+				<button>
+					<RxSpeakerLoud />
+				</button>
+			</a>
+		</div>
+	);
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetics from "./Phonetics";
-import { RxSpeakerLoud } from "react-icons/rx";
 
 export default function SearchResults(props) {
 	if (props.results) {
 		return (
 			<div className="SearchResults">
-				<button>
-					<RxSpeakerLoud />
-				</button>
 				<h2> {props.results.word.toUpperCase()} </h2>
 				{props.results.phonetics.map(function (phonetics, index) {
 					return (
