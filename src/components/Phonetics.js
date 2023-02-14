6454c9c9bbbae4e369e8106ad0f818a33d253142
Elizabeth <tjeudy.e@gmail.com>
2023-02-14 14:48:07 -0500

    added audio component

--- src/components/Phonetics.js
+++ src/components/Phonetics.js
@@ -3,13 +3,16 @@ import "./Phonetics.css";
 import { HiPlay } from "react-icons/hi";
 
 export default function Phonetics(props) {
+	function playAudio() {
+		let audio = new Audio(props.phonetics.audio);
+		audio.play();
+	}
+
 	return (
 		<div className="Phonetics">
 			<span className="phoneticsText">{props.phonetics.text}</span>
 			<span className="phoneticsAudio">
-				<a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-					<HiPlay />
-				</a>
+				<HiPlay onClick={playAudio} />
 			</span>
 		</div>
 	);
