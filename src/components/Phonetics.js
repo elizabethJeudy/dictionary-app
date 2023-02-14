0da5a25c920e5d5d2afe5a0d9f6f4afba2641b5f
Elizabeth <tjeudy.e@gmail.com>
2023-02-14 09:38:58 -0500

    updated audio icon

--- src/components/Phonetics.js
+++ src/components/Phonetics.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { RxSpeakerLoud } from "react-icons/rx";
+import "./Phonetics.css";
+import { HiPlay } from "react-icons/hi";
 
 export default function Phonetics(props) {
+	let audio = new Audio(" ");
+	function playAudio(props) {
+		let audio = new Audio();
+		audio.play();
+	}
 	return (
 		<div className="Phonetics">
-			{props.phonetics.text}
-			<a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-				<RxSpeakerLoud />
-			</a>
+			<span className="phoneticsText">{props.phonetics.text}</span>
+			<span className="phoneticsAudio">
+				<a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+					<HiPlay />
+				</a>
+			</span>
 		</div>
 	);
 }
